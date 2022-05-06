a033136ebeb270422d22a4050932c3ce7bcc6048
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-05-06 15:36:16 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+int i;
+float massimo( float a[], int dim ){
+    float max= a[0];
+    for(i=0;i<dim; i++){
+        if( a[i]> max){
+            max= a[i];
+        }
+    }
+    return max;
+}
