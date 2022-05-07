21fe53fcdc556f3170db6e276e47cba29ada889d
ilariamerletti <100696314+ilariamerletti@users.noreply.github.com>
2022-05-07 14:00:25 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,10 @@
+#include "lib.h"
 int i;
-float massimo( float a[], int dim ){
-    float max= a[0];
+float massimo ( float B[], int dim ){
+    float max=0;
     for(i=0;i<dim; i++){
-        if( a[i]> max){
-            max= a[i];
+        if( B[i]> max){
+            max= B[i];
         }
     }
     return max;
